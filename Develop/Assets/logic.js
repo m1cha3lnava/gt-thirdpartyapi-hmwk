9e11cd1c831e284be9d404c6c5f12ac20cf1584c
Michael Nava <M1cha3l.nava@gmail.com>
2020-06-15 20:51:15 -0400

    finished logic for day planner slots

--- Develop/Assets/logic.js
+++ Develop/Assets/logic.js
@@ -1,6 +1,8 @@
 //Initialize variables
-console.log("java");
-var currentDay = 0;
+//current day and time
+var currentDay = new Date();
+var currentHour = currentDay.getHours();
+// hour object for the if statements
 var hourConversion = [
   { regHour: "9AM", utc: "9" },
   { regHour: "10AM", utc: "10" },
@@ -12,8 +14,9 @@ var hourConversion = [
   { regHour: "4PM", utc: "16" },
   { regHour: "5PM", utc: "17" },
 ];
+console.log("today is " + currentDay);
+console.log("the current hour is " + currentHour);
 /* 
-    current day and time
     past events 
     current event
     future event */
@@ -22,11 +25,22 @@ var hourConversion = [
 function createSlots() {
   /* create time slots */
   var newSlot = $("<div>").addClass("row");
-  var timeCol = $("<div>").addClass("col-1 hour").text(hourConversion[i].regHour);
+  var timeCol = $("<div>")
+    .addClass("col-1 hour")
+    .text(hourConversion[i].regHour);
   var plannerCol = $("<textarea>").addClass("col-10 ");
   var saveCol = $("<button>").addClass("col-1 saveBtn").text("Save");
   newSlot.append(timeCol).append(plannerCol).append(saveCol);
   $(".container").append(newSlot);
+  console.log(hourConversion[i].utc);
+  console.log(currentHour);
+  if (hourConversion[i].utc == currentHour) {
+    plannerCol.addClass("present");
+  } else if (hourConversion[i].utc < currentHour) {
+    plannerCol.addClass("past");
+  } else {
+    plannerCol.addClass("future");
+  }
 }
 
 /* add content into the time slots */
@@ -39,8 +53,6 @@ function createSlots() {
 //Function calls
 for (var i = 0; i < 9; i++) {
   createSlots();
-  console.log(hourConversion[i].regHour);
-  // if (hoursArray[i])
 }
 
 //Event listeners
